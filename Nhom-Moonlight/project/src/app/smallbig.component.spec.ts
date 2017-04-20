fe7b251bac4d74e737a38e9914a43b567a6a3264
Cuong97CC <osident1997@gmail.com>
2017-04-20 22:23:14 +0700

    Update smallbig.component.spec.ts

--- Nhom-Moonlight/project/src/app/smallbig.component.spec.ts
+++ Nhom-Moonlight/project/src/app/smallbig.component.spec.ts
@@ -5,7 +5,7 @@ import { SBComponent } from './smallbig.component';
 import { By }              from '@angular/platform-browser';
 import { DebugElement }    from '@angular/core';
 
-describe('AppComponent', () => {
+describe('Sắp xếp các số', () => {
   beforeEach(() => {
     jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
     TestBed.configureTestingModule({
