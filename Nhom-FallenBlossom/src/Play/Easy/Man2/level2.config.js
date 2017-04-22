7f11b065545a981697123dce5a7a60ef24dbfeb5
tuananh97 <tuananhk60uet@gmail.com>
2017-04-22 10:19:05 +0700

    Cap nhat du an

---
+++ Nhom-FallenBlossom/src/Play/Easy/Man2/level2.config.js
@@ -0,0 +1,46 @@
+var typeOfQuestions = new Array();
+var questions = new Array();
+var choices = new Array();
+var answers = new Array();
+var response = new Array();
+
+questions[0] = "12 + 15 = ? ";
+typeOfQuestions[0] = "";
+choices[0] = new Array();
+choices[0][0] = "24";
+choices[0][1] = "25";
+choices[0][2] = "23";
+choices[0][3] = "27";
+answers[0] = choices[0][3];
+
+questions[1] = "3 x (6 + 4) : (7-2) = ?"
+typeOfQuestions[1] = "";
+choices[1] = new Array();
+choices[1][0] = "4";
+choices[1][1] = "5";
+choices[1][2] = "6";
+choices[1][3] = "7";
+answers[1] = choices[1][2];
+
+
+questions[2] = "11  *  12 = ??"
+typeOfQuestions[2] = "";
+choices[2] = new Array();
+choices[2][0] = "151";
+choices[2][1] = "142";
+choices[2][2] = "132";
+choices[2][3] = "163";
+answers[2] = choices[2][2];
+
+questions[3] = "5 x 6 + 4 x 5 = ?"
+typeOfQuestions[3] = "";
+choices[3] = new Array();
+choices[3][0] = "60";
+choices[3][1] = "70";
+choices[3][2] = "50";
+choices[3][3] = "80";
+answers[3] = choices[3][2];
+
+
+
+
