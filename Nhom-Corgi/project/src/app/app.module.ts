e25b2455dd432a9e19b58e8851928b16b0fe4eae
huyhoc1310 <huyhoc1310@gmail.com>
2017-04-22 22:20:55 +0700

    Cập nhật dự án nhóm

--- Nhom-Corgi/project/src/app/app.module.ts
+++ Nhom-Corgi/project/src/app/app.module.ts
@@ -4,16 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home.component';
 import { OnlineTestComponent } from './onlinetest.component';
+import {appRoutes} from './app.routes';
 @NgModule({
   declarations: [
     AppComponent,
-    OnlineTestComponent
+    OnlineTestComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    appRoutes
   ],
   providers: [],
   bootstrap: [AppComponent]
