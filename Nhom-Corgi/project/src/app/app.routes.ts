bfcfdbec00a50948c4a60de86f9a7b9aea57f553
huyhoc1310 <huyhoc1310@gmail.com>
2017-04-23 16:22:23 +0700

    cập nhật lại dự án nhóm

--- Nhom-Corgi/project/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home.component';
-import { OnlineTestComponent } from './onlinetest.component';
-
-const routing: Routes = [
-    { path: '', component: HomeComponent},
-    { path: 'onlinetest', component: OnlineTestComponent}
-]
-export const appRoutes = RouterModule.forRoot(routing);
