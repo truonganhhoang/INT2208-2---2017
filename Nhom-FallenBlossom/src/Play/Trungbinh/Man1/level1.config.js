06db787506ef5b91db97fad6eb9ba8b150296b65
tuananh97 <tuananhk60uet@gmail.com>
2017-05-10 10:57:25 +0700

    Cap nhat code moi

---
+++ Nhom-FallenBlossom/src/Play/Trungbinh/Man1/level1.config.js
@@ -0,0 +1,53 @@
+var typeOfQuestions = new Array();
+var questions = new Array();
+var choices = new Array();
+var answers = new Array();
+var response = new Array();
+
+questions[0] = "3 x (121 + 4) : (61 - 36)"
+typeOfQuestions[0] = "";
+choices[0] = new Array();
+choices[0][0] = "16";
+choices[0][1] = "14";
+choices[0][2] = "13";
+choices[0][3] = "15";
+answers[0] = choices[0][3];
+
+questions[1] = "3 x (6 + 4) : (7-2) = ?"
+typeOfQuestions[1] = "";
+choices[1] = new Array();
+choices[1][0] = "4";
+choices[1][1] = "5";
+choices[1][2] = "6";
+choices[1][3] = "7";
+answers[1] = choices[1][2];
+
+
+questions[2] = "11  *  12 = ??"
+typeOfQuestions[2] = "";
+choices[2] = new Array();
+choices[2][0] = "151";
+choices[2][1] = "142";
+choices[2][2] = "132";
+choices[2][3] = "163";
+answers[2] = choices[2][2];
+
+questions[3] = "7 x 6 + 3 x 5 = ?"
+typeOfQuestions[3] = "";
+choices[3] = new Array();
+choices[3][0] = "55";
+choices[3][1] = "56";
+choices[3][2] = "57";
+choices[3][3] = "58";
+answers[3] = choices[3][2];
+
+questions[4] = "144 : (36 - 4 x 6)"
+typeOfQuestions[4] = "";
+choices[4] = new Array();
+choices[4][0] = "12";
+choices[4][1] = "13";
+choices[4][2] = "14";
+choices[4][3] = "15";
+answers[4] = choices[4][0];
+
+
