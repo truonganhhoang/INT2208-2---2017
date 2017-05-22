01e7c29269b0022a644098a21f8a03f7c8c2c235
hunghuy1907 <hunghuy1907@gmail.com>
2017-05-21 21:21:49 -0700

    Cap nhat unitest

---
+++ Nhom-FallenBlossom/unit-test/README.md
@@ -0,0 +1,18 @@
+AngularJS Unit-testing Reference Code
+================================================
+
+Demo project for AngularJS-il meetup #3.
+how to unit test AngularJS components with jasmine and karma.
+
+
+To run this Demo:
+-----------------
+
+1. run npm install (for karma runner and plugins)
+2. run bower install (for angular and angular mocks)
+3. webstorm users: right click on karma.conf.js and choose run
+4. non webstorm users: run karma from the command line (require karma-cli to be installed)
+
+
+#### Link to slides [view slides](http://www.slideshare.net/nirkaufman/angularjs-unit-testing)
+#### need support with this demo? [nirkaufman@gmail.com](nirkaufman@gmail.com)
---
+++ Nhom-FallenBlossom/unit-test/src/level1.config.js
@@ -0,0 +1,57 @@
+var typeOfQuestions = new Array();
+var questions = new Array();
+var choices = new Array();
+var answers = new Array();
+var response = new Array();
+
+questions[0] = "5 + 6 = ? ";
+typeOfQuestions[0] = "";
+choices[0] = new Array();
+choices[0][0] = "8";
+choices[0][1] = "10";
+choices[0][2] = "11";
+choices[0][3] = "12";
+answers[0] = choices[0][2];
+
+questions[1] = "(4 x 5 + 2 ) :  11= ?"
+typeOfQuestions[1] = "";
+choices[1] = new Array();
+choices[1][0] = "1";
+choices[1][1] = "2";
+choices[1][2] = "3";
+choices[1][3] = "5";
+answers[1] = choices[1][1];
+
+
+questions[2] = "11  +  12 = ??"
+typeOfQuestions[2] = "";
+choices[2] = new Array();
+choices[2][0] = "22";
+choices[2][1] = "25";
+choices[2][2] = "23";
+choices[2][3] = "26";
+answers[2] = choices[2][2];
+
+questions[3] = "http://i.imgur.com/oSW02ZJ.jpg";
+typeOfQuestions[3] = " ";
+choices[3] = new Array();
+choices[3][0] = "5";
+choices[3][1] = "6";
+choices[3][2] = "4";
+choices[3][3] = "1";
+answers[3] = choices[3][0];
+
+questions[4] = "http://i.imgur.com/49BGyGU.jpg";
+typeOfQuestions[4] = " ";
+choices[4] = new Array();
+choices[4][0] = "5";
+choices[4][1] = "6";
+choices[4][2] = "4";
+choices[4][3] = "1";
+answers[4] = choices[4][2];
+
+
+
+
+
+
